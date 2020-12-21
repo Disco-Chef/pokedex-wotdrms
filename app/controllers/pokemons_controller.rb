61d161b53d672709b658a3070f24ebba8a51bf6a
Nikos Agathos <nicolas.agathos@gmail.com>
2020-12-21 01:48:02 +0100

    ok so far

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,21 @@
 class PokemonsController < ApplicationController
   before_action :set_pokemon, only: [:show]
   def index
-    @pokemons = Pokemon.all
+    if params[:search].present?
+      name_is_present = params[:search]["name"].present?
+      type_is_present = params[:search]["type"].present?
+      if name_is_present && type_is_present
+        @pokemons = Pokemon.filter_by_name_and_type(name, type)
+      elsif name_is_present && !type_is_present
+        @pokemons = Pokemon.filter_by_name(name)
+      elsif !name_is_present && type_is_present
+        @pokemons = Pokemon.filter_by_type(type)
+      else
+        @pokemons = Pokemon.all
+      end
+    else
+      @pokemons = Pokemon.all
+    end
   end
 
   def show
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,18 @@
 class Pokemon < ApplicationRecord
   has_many :pokemon_types
   has_many :types, through: :pokemon_types
+
+  def self.filter_by_name(name)
+    sql_query = "pokemons.name ILIKE :name"
+    return Pokemon.where(sql_query, name: "%#{name}%").order(:id)
+  end
+
+  def self.filter_by_type(type)
+    return Type.find_by(name: type).pokemons
+  end
+
+  def self.filter_by_name_and_type(name, type)
+    sql_query = "pokemons.name ILIKE :name"
+    return Type.find_by(name: type).pokemons.where(sql_query, name: "%#{name}%").order(:id)
+  end
 end
