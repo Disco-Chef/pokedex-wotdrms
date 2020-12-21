39a62904d0ad43f7ed2106b6612c488575ca98f6
Nikos Agathos <nicolas.agathos@gmail.com>
2020-12-21 05:26:45 +0100

    come on sanity

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # last runs [266.666, 277.563, 141.108, 124.887, 112.916, 123.974]
 Pokemon.destroy_all
 Type.destroy_all
-EvolutionChain.destroy_all
+# EvolutionChain.destroy_all
 
-start_time = Time.now
+# start_time = Time.now
 puts "Creating Types..."
 CreateFromPokeapi.new.create_types
 puts ""
 puts "Creating Pokemon..."
 CreateFromPokeapi.new.create_pokemon
 puts ""
-puts "Creating Evolution Chains..."
-CreateFromPokeapi.new.create_evolution_chains
-end_time = Time.now
-puts ""
-puts "It took: #{end_time - start_time} seconds this time"
+# puts "Creating Evolution Chains..."
+# CreateFromPokeapi.new.create_evolution_chains
+# end_time = Time.now
+# puts ""
+# puts "It took: #{end_time - start_time} seconds this time"
